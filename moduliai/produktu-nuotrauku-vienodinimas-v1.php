d4ab3a560be105cb161d4809f243d5a6926a8acf
raimis079-creator <raimis079@gmail.com>
2026-07-13 18:03:13 +0300

    imgfix v2 (+build-a-box)

--- moduliai/produktu-nuotrauku-vienodinimas-v1.php
+++ moduliai/produktu-nuotrauku-vienodinimas-v1.php
@@ -1,13 +1,15 @@
 if ( ! defined( 'ABSPATH' ) ) { return; }
 
 /**
- * Petshop Produktu Nuotrauku Vienodinimas v1
- * WC thumbnail = uncropped -> skirtingi native aspect ratios -> skirtingi aukščiai
- * loop'e (shop/kategorija/panašūs/cross-sell). Fix: 1:1 box + object-fit:contain.
+ * Petshop Produktu Nuotrauku Vienodinimas v2
+ * WC thumbnail = uncropped -> skirtingi native aspect ratios -> skirtingi aukščiai.
+ * Fix: 1:1 box + object-fit:contain visose produktų loop'o formose.
+ * v2: pridėta build-a-box (Mix and Match) prekių eilutės (.mnm_child_product_images).
  * Nieko neapkarpo (rinkinių plačios nuotraukos lieka pilnos), tik vienodina aukštį.
  */
 add_action( 'wp_head', function () {
 	echo '<style id="ps-img-uniform">
+	/* Shop/kategorija/panašūs/cross-sell loop */
 	.product-small .box-image img,
 	ul.products li.product .box-image img,
 	.related .product-small .box-image img,
@@ -22,6 +24,25 @@ add_action( 'wp_head', function () {
 	ul.products li.product .box-image{
 		aspect-ratio:1/1 !important;
 		display:block !important;
+	}
+	/* Build-a-box (Mix and Match) prekių eilutės */
+	.mnm_child_product_images{
+		width:90px !important;
+		aspect-ratio:1/1 !important;
+		display:block !important;
+	}
+	.mnm_child_product_images .mnm_child_product_image,
+	.mnm_child_product_images figure{
+		width:100% !important;
+		height:100% !important;
+		margin:0 !important;
+	}
+	.mnm_child_product_images img{
+		aspect-ratio:1/1 !important;
+		width:100% !important;
+		height:100% !important;
+		object-fit:contain !important;
+		object-position:center !important;
 	}
 	</style>' . "\n";
 }, 99 );
