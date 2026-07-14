23e27e807ed44c63c2ae4a31e736769d032e326c
raimis079-creator <raimis079@gmail.com>
2026-07-14 18:13:24 +0300

    S193: core v0.7.0 (Refill Engine M11)

--- plugins/petshop-core/petshop-core.php
+++ plugins/petshop-core/petshop-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Petshop Core
  * Description: Petshop.lt provider-neutralus sistemos pamatas: event log + retry queue, event registry, consent log/sync, action tokens, message provider interface. Prielaida: bet koks message provider (Sender, SMS, kt.) priklauso nuo šio plugin'o, ne atvirkščiai.
- * Version: 0.6.0
+ * Version: 0.7.0
  * Author: UAB Avesa / Petshop.lt
  * Requires at least: 6.0
  * Requires PHP: 8.1
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PETSHOP_CORE_VERSION', '0.6.0' );
+define( 'PETSHOP_CORE_VERSION', '0.7.0' );
 define( 'PETSHOP_CORE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PETSHOP_CORE_URL', plugin_dir_url( __FILE__ ) );
 
@@ -73,6 +73,7 @@ require_once PETSHOP_CORE_DIR . 'includes/class-event-registry.php';
 require_once PETSHOP_CORE_DIR . 'includes/class-event-emitters.php';
 require_once PETSHOP_CORE_DIR . 'includes/class-magic-login.php';
 require_once PETSHOP_CORE_DIR . 'includes/class-pet-profile.php';
+require_once PETSHOP_CORE_DIR . 'includes/class-refill-engine.php';
 
 // --- Activation ---
 register_activation_hook( __FILE__, function() {
@@ -81,6 +82,7 @@ register_activation_hook( __FILE__, function() {
 	Petshop_Action_Tokens::install();
 	Petshop_Action_Tokens::ensure_keys();
 	Petshop_Pet_Profile::install();
+	Petshop_Refill_Engine::install();
 	if ( ! wp_next_scheduled( 'ps_esp_cron_process_pending' ) ) {
 		wp_schedule_event( time() + 300, 'ps_esp_5min', 'ps_esp_cron_process_pending' );
 	}
@@ -108,6 +110,7 @@ add_action( 'plugins_loaded', function() {
 	Petshop_Consent_Log::maybe_install();
 	Petshop_Action_Tokens::maybe_install();
 	Petshop_Pet_Profile::maybe_install();
+	Petshop_Refill_Engine::maybe_install();
 
 	// MIGRACIJOS APSAUGA (S186): jei petshop-esp v0.3.0 dar gyvas su savo Retry_Queue
 	// ir Consent_Sync klasemis, NELEIDZIAM core registruoti tuos pacius hook'us
@@ -140,6 +143,9 @@ add_action( 'plugins_loaded', function() {
 add_action( 'woocommerce_init', function() {
 	if ( class_exists( 'Petshop_Event_Emitters' ) ) {
 		Petshop_Event_Emitters::init();
+	}
+	if ( class_exists( 'Petshop_Refill_Engine' ) ) {
+		Petshop_Refill_Engine::init();
 	}
 } );
 
